d09ac7aacc279d4ffa63ec02bed4cd3e3cd2c28e
Nency <rathodnency64@gmail.com>
2026-09-26 22:45:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: './', // Crucial for making sure Vercel finds the bundled CSS and JS files relative to index.html
   plugins: [react()],
   resolve: {
     alias: {
