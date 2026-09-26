70bbc76f31ec7070890c2838c6464006f3895d21
Nency <rathodnency64@gmail.com>
2026-09-26 22:43:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: './', // Ensures assets use relative paths so Vercel loads your CSS and JS correctly
   plugins: [react()],
   resolve: {
     alias: {
